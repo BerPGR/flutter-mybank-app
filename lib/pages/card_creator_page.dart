45edf5e878a45845d554119367309c8d7293be97
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-02-10 17:55:44 -0300

    Added navigation

---
+++ lib/pages/card_creator_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CardCreatorPage extends StatefulWidget {
+  const CardCreatorPage({super.key});
+
+  @override
+  State<CardCreatorPage> createState() => _CardCreatorPageState();
+}
+
+class _CardCreatorPageState extends State<CardCreatorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mybank/pages/card_creator_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -14,14 +15,37 @@ class _HomePageState extends State<HomePage> {
       body: Container(
         child: SafeArea(
           child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Text('Your personal bank'),
-                  ],
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'My personal bank',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              color: Colors.grey[300],
+                              borderRadius: BorderRadius.circular(25)),
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => CardCreatorPage()));
+                            },
+                            icon: Icon(Icons.add),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
