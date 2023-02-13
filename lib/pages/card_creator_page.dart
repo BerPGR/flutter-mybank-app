9f0dc1e8eb60dc31b9c49aa220f919ffa5f28857
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-02-13 16:16:12 -0300

    Created inputs and button for adding card

--- lib/pages/card_creator_page.dart
+++ lib/pages/card_creator_page.dart
@@ -9,6 +9,15 @@ class CardCreatorPage extends StatefulWidget {
 }
 
 class _CardCreatorPageState extends State<CardCreatorPage> {
+  List<String> _bancos = [
+    'Caixa',
+    'Bradesco',
+    'Itaú',
+    'Banco do Brasil',
+    'NuBank',
+    'Inter'
+  ];
+  String _banco = '';
   TextEditingController _numberController = TextEditingController();
   TextEditingController _monthController = TextEditingController();
   TextEditingController _yearController = TextEditingController();
@@ -18,33 +27,126 @@ class _CardCreatorPageState extends State<CardCreatorPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                child: Text(
-                  'Add your card info',
-                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Stack(
+          children: [
+            SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 20),
+                    child: Text(
+                      'Add your card info',
+                      style:
+                          TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  DropdownButton<String>(
+                    elevation: 8,
+                    hint: Text('Choose your bank!'),
+                    value: _banco.isNotEmpty ? _banco : null,
+                    onChanged: (value) {
+                      setState(() {
+                        _banco = value.toString();
+                      });
+                    },
+                    items: _bancos.map(
+                      (String value) {
+                        return DropdownMenuItem(
+                          value: value,
+                          child: Text(value),
+                        );
+                      },
+                    ).toList(),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextField(
+                    controller: _numberController,
+                    keyboardType: TextInputType.number,
+                    maxLength: 16,
+                    decoration: InputDecoration(
+                      labelText: 'Number',
+                      labelStyle: TextStyle(color: Colors.deepPurple),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(35),
+                        borderSide: BorderSide(
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: (MediaQuery.of(context).size.width * 0.4),
+                        child: TextField(
+                          controller: _monthController,
+                          keyboardType: TextInputType.number,
+                          maxLength: 2,
+                          decoration: InputDecoration(
+                            labelText: 'Month',
+                            labelStyle: TextStyle(color: Colors.deepPurple),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35),
+                              borderSide: BorderSide(
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: (MediaQuery.of(context).size.width * 0.4),
+                        child: TextField(
+                          controller: _yearController,
+                          keyboardType: TextInputType.number,
+                          maxLength: 2,
+                          decoration: InputDecoration(
+                            labelText: 'Year',
+                            labelStyle: TextStyle(color: Colors.deepPurple),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(35),
+                              borderSide: BorderSide(
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              bottom: 10,
+              child: ElevatedButton(
+                onPressed: _verificaValores,
+                child: Text('Add Card'),
+                style: ElevatedButton.styleFrom(
+                  elevation: 2,
+                  fixedSize: Size.fromWidth(MediaQuery.of(context).size.width),
                 ),
               ),
-              TextField(
-                controller: _numberController,
-                decoration: InputDecoration(labelText: 'Number'),
-              ),
-              TextField(
-                controller: _monthController,
-                decoration: InputDecoration(labelText: 'Month'),
-              ),
-              TextField(
-                controller: _yearController,
-                decoration: InputDecoration(labelText: 'Year'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
